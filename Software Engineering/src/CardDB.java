260bf5513ee327f7df670bd2103f0ffc7f5c817c
Tyler <Tyler@ecc224.net>
2018-12-13 20:54:02 -0600

    CSV parser
    I started to work on the csv parser for the program. I think that I have
    a working parser (in theory), and now, I need to start evaluating the
    information that was pared.

--- Software Engineering/src/CardDB.java
+++ Software Engineering/src/CardDB.java
@@ -1,14 +1,47 @@
 import java.awt.*;
 import java.io.*;
+import java.util.ArrayList;
 
 import javax.swing.*;
 
 public class CardDB {
-	private String file = "cards.csv";
-	
+	private String file = "cardDB.csv";
+	private ArrayList<String> rows = new ArrayList<String>(); //This an array list of the individual rows in the table
+	private String[] colHeadings;
 	public CardDB() throws FileNotFoundException {
-		BufferedReader br = new BufferedReader(new FileReader(file));
 		
-		br.readLine();
+		//This section reads data from the file and stores it in an arrayList
+		try (BufferedReader br = new BufferedReader(new FileReader(file))){
+			String topLine = br.readLine();
+			this.colHeadings = topLine.split(",");
+			
+			while (br.ready()) {
+				ArrayList<String> row = new ArrayList<String>();
+				
+				String tmpCellHolder = ""; //This is used to hold data if a cell contains a comma and that comma isn't the end of the cell.
+				String line = br.readLine();
+				String[] cells = line.split(","); //Splits the read line into an array divided from the commas
+				for (int i = 0; i < cells.length; i++) {
+					String cell = cells[i];
+					String lastChar = cell.substring(cell.length() - 1); //Last character in cell
+					String secondChar = cell.substring(cell.length() - 2); //Second to last character in cell
+					if (lastChar.equals("\\") && !secondChar.equals("\\")) { //Checks to see if the , was escaped or if the \ was escaped!
+						tmpCellHolder += "," + cell;
+					} else {
+						if (!tmpCellHolder.equals("")) {
+							row.add(tmpCellHolder);
+							tmpCellHolder = "";
+						} else {
+							row.add(cell);
+						}
+					}
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 	}
 }
